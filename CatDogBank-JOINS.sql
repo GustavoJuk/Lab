ca7cab486222f3c1bf0256d94b52163e4c07360f
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-10-21 08:47:28 -0300

    JOINS
    Join de relação entre Cliente e produto criada

--- CatDogBank-JOINS.sql
+++ CatDogBank-JOINS.sql
@@ -1,5 +1,9 @@
 USE catdog_bank;
 
+/*Relação cliente-produto*/
+SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.produto FROM Carteira_Produto
+INNER JOIN Cliente ON Carteira_Produto.produto = Cliente.id;
+
 /*Relação cliente-adesao*/
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.adesao_data FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id;
