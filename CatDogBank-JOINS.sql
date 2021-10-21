2c02c181294d939545d9800c9742ee476ddecadf
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-10-21 08:24:29 -0300

    JOINS
    Join de relação com cliente e saldo ordenado em crescente de acordo com o saldo e com saldo maior que zero

--- CatDogBank-JOINS.sql
+++ CatDogBank-JOINS.sql
@@ -8,3 +8,7 @@ INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id;
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.cancelamento_data FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produto.cancelamento_data IS NOT NULL;
 
+/*Relação cliente-saldo*/
+SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.saldo FROM Carteira_Produto
+INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produto.saldo IS NOT NULL AND Carteira_Produto.saldo > 0
+ORDER BY Carteira_Produto.saldo;
