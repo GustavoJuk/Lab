62426b7d352d39f992a456f015dedc05dea4ac69
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-10-21 08:20:33 -0300

    JOINS
    Joins de relação com o cliente e a data de cancelamento sem NULL criada

--- CatDogBank-JOINS.sql
+++ CatDogBank-JOINS.sql
@@ -2,3 +2,6 @@ USE catdog_bank;
 
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.adesao_data FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id;
+
+SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.cancelamento_data FROM Carteira_Produto
+INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produto.cancelamento_data IS NOT NULL;
