3c8735b0a47699a0e933f0b2cf5fe5683a82dd74
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-10-21 08:21:39 -0300

    Comentários

--- CatDogBank-JOINS.sql
+++ CatDogBank-JOINS.sql
@@ -1,7 +1,10 @@
 USE catdog_bank;
 
+/*Relação cliente-adesao*/
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.adesao_data FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id;
 
+/*Relação cliente-cancelamento*/
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.cancelamento_data FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produto.cancelamento_data IS NOT NULL;
+
