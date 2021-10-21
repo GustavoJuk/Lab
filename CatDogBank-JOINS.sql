e80993f2a9799f8a03a79f0931a7b761ae28c021
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-10-21 08:29:12 -0300

    JOINS
    Joins de relação entre cliente e filial ordenado pelo bairro do cliente

--- CatDogBank-JOINS.sql
+++ CatDogBank-JOINS.sql
@@ -12,3 +12,8 @@ INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produ
 SELECT Carteira_Produto.id, Cliente.cliente_nome, Carteira_Produto.saldo FROM Carteira_Produto
 INNER JOIN Cliente ON Carteira_Produto.cliente = Cliente.id WHERE Carteira_Produto.saldo IS NOT NULL AND Carteira_Produto.saldo > 0
 ORDER BY Carteira_Produto.saldo;
+
+/*Relação cliente-filial*/
+SELECT Cliente.id, Cliente.cliente_nome, Cliente.filial, Cliente.bairro FROM Cliente
+INNER JOIN Filial ON Cliente.id = Filial.id
+ORDER BY Cliente.bairro;
