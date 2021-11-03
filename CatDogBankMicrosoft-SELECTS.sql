2a0f680a2583d16294480e33e0fdeb09ae7eebd7
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-11-03 20:42:32 -0300

    Microsoft SGBD
    Transferência para o banco de dados do Microsoft feita

---
+++ CatDogBankMicrosoft-SELECTS.sql
@@ -0,0 +1,19 @@
+USE catdog_bank;
+
+SELECT * FROM Cargo
+order by id;
+
+SELECT * FROM Filial
+order by numero;
+
+SELECT * FROM Funcionario
+order by id;
+
+SELECT * FROM Produto
+order by id;
+
+SELECT * FROM Cliente
+order by cliente_nome;
+
+SELECT * FROM Carteira_Produto
+order by id;
---
+++ CatDogBankMicrosoft.sql
@@ -0,0 +1,92 @@
+CREATE DATABASE IF NOT EXISTS catdog_bank;
+USE catdog_bank;
+
+CREATE TABLE IF NOT EXISTS Cargo (
+	id INT NOT NULL,
+	cargo VARCHAR(25) NOT NULL,
+	salario DECIMAL(10,2) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Filial (
+	id INT NOT NULL,
+	nome_filial VARCHAR(80) NOT NULL,
+	logradouro VARCHAR(50) NOT NULL,
+	numero VARCHAR(5) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    cep CHAR(8) NOT NULL,
+    
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Funcionario (
+	id INT NOT NULL,
+	empregado_nome VARCHAR(80) NOT NULL,
+	genero CHAR(1) NOT NULL,
+	ddd CHAR(2) NOT NULL,
+	celular CHAR(9) NOT NULL,
+	admissao DATE NOT NULL,
+    demissao DATE,
+    cargo INT NOT NULL,
+    filial INT NOT NULL, 
+	nascimento DATE NOT NULL,
+	estado_civil char(1) NOT NULL,
+	filhos INT NOT NULL,
+	pcd BOOL NOT NULL,
+	logradouro VARCHAR(50) NOT NULL,
+	numero_endereco VARCHAR(5) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    cep CHAR(8) NOT NULL,
+
+	FOREIGN KEY (cargo) REFERENCES Cargo(id),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Produto (
+	id INT NOT NULL,
+	nome_produto VARCHAR(25) NOT NULL,
+
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Cliente (
+	id INT AUTO_INCREMENT NOT NULL,
+	cliente_nome VARCHAR(80) NOT NULL,
+	genero CHAR(1) NOT NULL,
+	ddd CHAR(2) NOT NULL,
+	celular CHAR(9) NOT NULL,
+	inicio_relacionamento DATE NOT NULL,
+    fim_relacionamento DATE,
+    filial INT NOT NULL,
+    nascimento DATE NOT NULL,
+	estado_civil char(1) NOT NULL,
+    logradouro VARCHAR(50) NOT NULL,
+	numero_endereco VARCHAR(5) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    uf CHAR(2) NOT NULL,
+    cep CHAR(8) NOT NULL,
+    
+    FOREIGN KEY (filial) REFERENCES Filial(id),
+    
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Carteira_Produto (
+	id INT AUTO_INCREMENT NOT NULL,
+    cliente INT NOT NULL,
+	produto INT NOT NULL,
+    adesao_data DATE NOT NULL,
+    cancelamento_data DATE,
+    saldo DOUBLE NOT NULL,
+
+	FOREIGN KEY (cliente) REFERENCES Cliente(id),
+    FOREIGN KEY (produto) REFERENCES Produto(id),
+    
+    PRIMARY KEY (id)
+);
