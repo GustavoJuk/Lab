cd4cea46ee4f32ab5b57efff6efac5ca7965d71b
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-11-08 21:02:50 -0300

    VIEW
    Correção nos inserts e criação da View por completo

--- CatDogBank-VIEW.sql
+++ CatDogBank-VIEW.sql
@@ -1,9 +1,8 @@
 USE catdog_bank;
 
-SELECT * FROM Funcionario
+GO
 
-CREATE VIEW ViewFuncionarios 
-AS 
-SELECT id, empregado_nome, genero, ddd, celular, admissao, demissao, cargo, filial, nascimento, estado_civil, filhos, pcd, logradouro, numero_endereco, bairro, cidade, uf, cep FROM Funcionario;
-
-SELECT * FROM ViewFuncionarios
+CREATE VIEW View_Funcionarios AS 
+SELECT id, empregado_nome, genero, ddd, celular, admissao, demissao, cargo, filial, nascimento, estado_civil, filhos, pcd, logradouro, numero_endereco, bairro, cidade, uf, cep 
+FROM Funcionario
+WHERE cargo = 1;
