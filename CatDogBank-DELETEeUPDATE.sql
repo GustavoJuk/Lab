388c0ab6da0a4fea8e787ab005bdfb1735c6b7b1
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-11-10 20:10:35 -0300

    UPDATE & DELETE

---
+++ CatDogBank-DELETEeUPDATE.sql
@@ -0,0 +1,9 @@
+USE catdog_bank;
+
+/*Deleção e atualização de funcionarios*/
+DELETE FROM Funcionario WHERE demissao != NULL;
+UPDATE Funcionario SET demissao = '2021-11-10' WHERE empregado_nome = 'Grasyellen Santos';
+
+/*Atualização quanto ao tipo de produto e data de cancelamento*/
+UPDATE Carteira_Produto SET produto = 1 WHERE produto = 3 AND cancelamento_data = NULL;
+UPDATE Carteira_Produto SET cancelamento_data = '2021-11-10' WHERE produto = 3 AND cancelamento_data = NULL;
