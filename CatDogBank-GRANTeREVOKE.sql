bd35ec4c1478094910098fcfed44874770553284
GustavoJuk <77924969+GustavoJuk@users.noreply.github.com>
2021-11-10 20:26:35 -0300

    GRANT & REVOKE

---
+++ CatDogBank-GRANTeREVOKE.sql
@@ -0,0 +1,15 @@
+USE catdog_bank;
+
+/*Criando o login*/
+CREATE LOGIN LOG_TBL
+WITH PASSWORD = '123456';
+
+/*Criando o usuário*/
+CREATE USER USER_TBL
+FOR LOGIN LOG_TBL;
+
+/*GRANT*/
+GRANT SELECT ON [dbo].[ADDRESS] TO USER_TBL;
+
+/*REVOKE*/
+REVOKE SELECT ON [dbo].[ADDRESS] TO USER_TBL;
